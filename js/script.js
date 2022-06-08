61e56e74780d27275ea152e3ff38205e1ff8ad34
rkhushi <khushi.cs.20@nitj.ac.in>
2022-06-08 22:18:30 +0530

    add files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*typing animation */
+var typed = new Typed(".typing",{
+    
+    strings: ['','Programmer.','','Programmer.',''],
+	typeSpeed: 100,
+	backSpeed:100,
+	loop: true,
+	loopCount: Infinity,
+	startDelay:500,
+});
